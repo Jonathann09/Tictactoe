b6f7b512807f9e9ce7e8a7feb1fbf0f515a433cc
Jonathan <jonathanurrete005@gmail.com>
2023-02-16 03:30:52 +0800

    Button 1

--- tictactoe.py
+++ tictactoe.py
@@ -28,3 +28,8 @@ label3=Label(root,text='PLAYER 1',font="Times 15 bold",bg='cadet blue',fg='black
 label3.grid(row=0,column=0,padx=5,pady=5,sticky='nsnew')
 
 label4=Label(root,text='PLAYER 2',font="Times 15 bold",bg='white',fg='black',bd=4,height=2,width=10,relief=RAISED,)
+
+#BUTTONS
+b1=Button(root,text='',font="Times 35 bold",bg='white',fg='black',width=12)
+b1.grid(row=1,column=0,padx=5,pady=5,sticky='snew')
+b1.config(command=lambda:ButtonClick(1))
